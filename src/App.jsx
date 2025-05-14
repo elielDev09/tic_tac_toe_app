3eda4854a752f56e0800453555d072f559a47a7c
elielDev09 <elielbesayp@gmail.com>
2025-05-14 09:12:21 +0100

    usar_styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import Game from './Square';
-import './App.css';
+import styled from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  #root {
+    width: 100%;
+    height: 80vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const StyleAppContainer = styled.div `
+  text-align: center;
+`
 
 function App() {
   return (
-    <div className='app-container '>
+    <>
+    <GlobalStyle />
+    <StyleAppContainer>
       <h1>Tic Tac Toe</h1>
       <Game /> {}
-    </div>
+    </StyleAppContainer>
+    </>
   );
 }
 
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,12 +1,41 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const StyleSquare = styled.button`
+    width: 80px;
+    height: 80px;
+    background-color: white;
+    border: 2px solid #555;
+    font-size: 2rem;
+    font-weight: bold;
+    cursor: pointer;
+    outline: none;
+    transition: background-color 0.2s;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    
+
+    &:hover {
+        background-color: #f0f0f0;
+    }
+`
+
+
 
 function Square( {value, onSquareClick} ){
     return (
-        <button className='square' onClick={onSquareClick}>
+        <StyleSquare onClick={onSquareClick}>
             {value}
-        </button>
+        </StyleSquare>
     )
 }
+
+const StyleBoardRow = styled.div `
+    display: flex;
+    text-align: center;
+`
 
 function Board({xIsNext,squares,onPlay}) {
     function handleClick(i){
@@ -32,22 +61,22 @@ function Board({xIsNext,squares,onPlay}) {
 
     return (
         <>
-            <div className='status'>{status}</div>
-            <div className="board-row">
+            <div>{status}</div>
+            <StyleBoardRow>
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
                 <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-            </div>
-            <div className="board-row">
+            </StyleBoardRow>
+            <StyleBoardRow>
                 <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
                 <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
                 <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-            </div>
-            <div className="board-row">
+            </StyleBoardRow>
+            <StyleBoardRow>
                 <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
                 <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
                 <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-            </div>
+            </StyleBoardRow>
         </>
     )
   }
@@ -85,11 +114,11 @@ function Board({xIsNext,squares,onPlay}) {
     })
 
     return (
-        <div className='game'>
-            <div className='game_board'>
+        <div>
+            <div>
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
             </div>
-            <div className='game-info'>
+            <div>
                 <ol>{moves}</ol>
             </div>
         </div>

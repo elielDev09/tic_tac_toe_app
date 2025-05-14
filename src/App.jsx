3a694ef127c2e1dee85758f9a440a4a373c24adf
elielDev09 <elielbesayp@gmail.com>
2025-05-14 08:13:12 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Game from './Square';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app-container '>
+      <h1>Tic Tac Toe</h1>
+      <Game /> {}
+    </div>
+  );
+}
+
+export default App;
